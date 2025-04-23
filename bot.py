c8d5092e667c95bd7a8eafc44730282bff6f22be
AnimeFlasher <anixanimeworld@gmail.com>
2025-04-23 11:45:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @Anime_Flasher")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -120,7 +120,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>- Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote>- Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Anime_Flasher</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -131,7 +131,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @Otakukart7")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
